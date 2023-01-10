83648c9de0d5c0e8e569a330e4e826d413822243
nata-vito <nata14vitorino@hotmail.com>
2023-01-10 16:56:30 -0300

    Performs written image reading, tests for image processing

--- src/scripts/tes.py
+++ src/scripts/tes.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 import rospy
 import cv2 as cv
-import pub_see_how as Pub
-import image_sub as imgSub
-#import hand_tracking as ht
+import hand_tracking as ht
 
-def leftHand(img):
-    i           = 0
-    #tracking    = ht.handDetector(detectionCon=0.75, maxHands=1, op='Left')
-    success     = 0
-  
-    #frame  = imgSub.image_converter().img
-
-    cv.imshow('test', img)
-    cv.waitKey(3) 
+def leftHand():
+    path = r'/home/rota2030/Documents/ws_see_how/src/see_how_pkg/src/scripts/img/imgTest.jpg'
+    
+    while cv.haveImageReader(path):
+        img = cv.imread(path, cv.IMREAD_ANYCOLOR)
+        cv.imshow('test', img)
+        cv.waitKey(3) 
 
     """ cv.imshow("test", imgSub.image_converter().img)
     cv.waitKey(3) """
 
 
-""" if __name__ == '__main__':
+if __name__ == '__main__':
     try:
         leftHand()
         
-        print("ok - left")
+        #print("ok - left")
     except rospy.ROSInterruptException:
-            pass """
+            pass
