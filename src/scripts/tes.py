6607017e0040c4dc22e115a69cfb4c0a075c1d28
nata-vito <nata14vitorino@hotmail.com>
2023-01-11 17:48:56 -0300

    Improved image preview from img_sub

--- src/scripts/tes.py
+++ src/scripts/tes.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 import rospy
 import cv2 as cv
+import subprocess
 import hand_tracking as ht
+
+def checkFile(imageFile):
+    try:
+        subprocess.run(["identify", "-regard-warnings", imageFile]).check_returncode()
+        return True
+    except (subprocess.CalledProcessError) as e:
+        return False
 
 def leftHand():
     path = r'/home/rota2030/Documents/ws_see_how/src/see_how_pkg/src/scripts/img/imgTest.jpg'
     
     while cv.haveImageReader(path):
-        img = cv.imread(path, cv.IMREAD_ANYCOLOR)
-        cv.imshow('test', img)
-        cv.waitKey(3) 
+        if checkFile(path) == True:
+            img = cv.imread(path, cv.IMREAD_ANYCOLOR)
+            cv.imshow('test', img)
+            cv.waitKey(20) 
 
     """ cv.imshow("test", imgSub.image_converter().img)
     cv.waitKey(3) """
