bfa0e62101a1f1e036cf2e63078c5d1ecff023b2
nata-vito <nata14vitorino@hotmail.com>
2023-01-19 11:14:06 -0300

    Logics for image processing changed and flow tensor initialization bug fixed

--- src/scripts/left_hand.py
+++ src/scripts/left_hand.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os
 import rospy
 import cv2 as cv
 import numpy as np
@@ -13,7 +12,6 @@ class LeftHand:
         self.i          = 0
         self.count      = 0
         self.tracking   = ht.handDetector(detectionCon=0.75, maxHands=1, op='Left')
-        #print(type(self.frame))
 
     def showImage(self, img):
         cv.imshow('Test', img)
@@ -21,24 +19,23 @@ class LeftHand:
         
     def imageCapture(self, img):
         self.frame = img
+        
         # Verify camera errors
         if(type(self.frame) == np.ndarray):
             self.isImg = True
+            
             # Flip frame to correct predict
             self.frame = cv.flip(self.frame, 1)
-            #self.showImage(self.frame)
         else:
            print("Error openning the image")
            self.isImg = False
                
     def HandCapture(self, img):
-        #print("here I am")
         
         # Capturing the first image to processing
         self.imageCapture(img)
 
         if self.isImg and not rospy.is_shutdown():
-            #print(self.isImg)
             
             # Hand's contour
             contour         = self.tracking.findHands(self.frame, op='Left') # OP select the hand to detect
@@ -50,37 +47,23 @@ class LeftHand:
                 
             num = self.tracking.labelText()  
             
-            if self.isImg:
-                font     = cv.FONT_HERSHEY_COMPLEX
-                left     = (50,50)
-                leftSt   = (50, 80)
-                right    = (380, 50)
-                rightSt  = (380, 80)
-                level    = str(self.tracking.levelOutput(self.frame)) + '%'
+            font     = cv.FONT_HERSHEY_COMPLEX
+            left     = (50,50)
+            leftSt   = (50, 80)
+            right    = (380, 50)
+            rightSt  = (380, 80)
+            level    = str(level) + '%'
 
-                if self.tracking.countFingers > -1:
-                    if self.tracking.label == 'Left':
-                        
-                        #print(self.tracking.handFingers, self.tracking.side,self.tracking.countFingers, self.tracking.op)
-                       
-                        # Pub Here
-                        #pub = Pub.Publisher(self.tracking.handFingers, self.tracking.side, self.tracking.countFingers, self.tracking.op)
-                        #pub.talker()
-                        
-                        """ print(tracking.mpHands)
-                        print(tracking.hands)
-                        
-                        print("ok") """
-                        
-                        cv.putText(self.frame, num, left, font, 1, (255,0,0), 2)
-                        cv.putText(self.frame, level, leftSt, font, 1, (255,0,0), 2)
-                    else:
-                        pass
-                        #cv.putText(self.frame, num, right, font, 1, (255,0,0), 2)
-                        #cv.putText(self.frame, level, rightSt, font, 1, (255,0,0), 2)
-        
-                cv.imshow('Frame', self.frame)
-                key = cv.waitKey(1)
+            if self.tracking.countFingers > -1 and self.tracking.label == 'Left':                   
+                cv.putText(self.frame, num, left, font, 1, (255,0,0), 2)
+                cv.putText(self.frame, level, leftSt, font, 1, (255,0,0), 2)
+
+            # Pub Here
+            pub = Pub.Publisher(self.tracking.handFingers, self.tracking.side, self.tracking.countFingers, self.tracking.op, self.tracking.levelOutput(self.frame))
+            pub.talker()
+            
+            cv.imshow('Frame', self.frame)
+            key = cv.waitKey(1)
         
 
         
