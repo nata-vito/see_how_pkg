6bf3189898158947fa002665e13e320f02892eba
nata-vito <nata14vitorino@hotmail.com>
2023-01-19 18:10:51 -0300

    Modified to receive data from any hand and send it to topics

--- src/scripts/pub_see_how.py
+++ src/scripts/pub_see_how.py
@@ -4,7 +4,7 @@ from std_msgs.msg import Header
 from see_how_pkg.msg import Hand
 
 class Publisher:
-    def __init__(self, fingers = 00000, side = 'None', countFingers = 0 , nodeName = 'Hand', level = 0):
+    def __init__(self, fingers = 00000, side = 'None', countFingers = 0 , nodeName = 'Hand', level = 0, X = 0, Y = 0, St_X = 0, St_Y = 0):
         
         self.fingers            = fingers
         self.side               = side
@@ -13,20 +13,30 @@ class Publisher:
         self.timestamp          = rospy.get_time()
         self.level              = int(level or 0)
         self.pub                = rospy.Publisher(self.nodeName, Hand, queue_size=10)
-
+        self.pose_X             = X
+        self.pose_Y             = Y
+        self.pose_St_X          = St_X
+        self.pose_St_Y          = St_Y
+        
     def talker(self):
         rate = rospy.Rate(100)
         rate.sleep()
     
         # Message construction
-        msg = Hand()
-        msg.header = Header(stamp = rospy.Time.now(), frame_id = 'odom')
-        msg.side = self.side
-        msg.fingers = int(self.fingers)
-        msg.countFingers = int(self.countFingers)
-        msg.nodeName = self.nodeName  
-        msg.level   = self.level
+        msg                 = Hand()
+        msg.header          = Header(stamp = rospy.Time.now(), frame_id = 'odom')
+        msg.side            = self.side
+        msg.fingers         = self.fingers
+        msg.countFingers    = int(self.countFingers)
+        msg.nodeName        = self.nodeName  
+        msg.level           = self.level
+        msg.pose_X          = self.pose_X
+        msg.pose_Y          = self.pose_Y
+        msg.pose_St_X       = self.pose_St_X
+        msg.pose_St_Y       = self.pose_St_Y
         
-        self.pub.publish(msg)
-
+        if self.nodeName == self.side:
+            self.pub.publish(msg)
+        else:
+            pass
 
