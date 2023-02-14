5bc03b71424bd48f85751b9521b39264dffebad4
nata-vito <nata14vitorino@hotmail.com>
2023-02-14 17:58:05 -0300

    Using URI as video source

--- src/scripts/cap_to_ros.py
+++ src/scripts/cap_to_ros.py
@@ -2,28 +2,29 @@
 
 import cv2
 import rospy
+from socket_nucleus import SockCLient as sck
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-print(0)
-# Video capture -> webcam
-cap = cv2.VideoCapture(0)
-
-# Verifying the camera input 
-print(cap.isOpened())
 
 # CvBridge to work with ROS
 bridge = CvBridge()
 
+# Video capture -> webcam
+cap = cv2.VideoCapture('http://172.17.57.144:8080/?action=stream')
+
+# Verifying the camera input 
+print(cap.isOpened())
+
 # Publishing Camera Image in /camera ROS Topic
 def talker():
-    pub = rospy.Publisher('/camera', Image, queue_size = 1)
+    pub                 = rospy.Publisher('/camera', Image, queue_size = 1)
     rospy.init_node('image', anonymous=True)
-    rate = rospy.Rate(10)
+    rate                = rospy.Rate(10)
+    #sock                = sck(ip = '127.0.1.1', port = 9999)
     
     while not rospy.is_shutdown():
-        
-        while True:
+        while True:   
             ret, frame = cap.read()
             #print(frame)
             
@@ -37,8 +38,21 @@ def talker():
                 break
             
             if rospy.is_shutdown():
-                cap.release()
-                
+                cap.release()         
+             
+            """ sock.clientResponse()   
+                     
+            if sock.frame.all():    
+                msg = bridge.cv2_to_imgmsg(sock.frame, "bgr8")
+                pub.publish(msg)
+        
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                sock.client_socket.close()
+                break
+            
+            if rospy.is_shutdown():
+                sock.client_socket.close()
+                 """
 if __name__ == "__main__":
     try:
         talker()
