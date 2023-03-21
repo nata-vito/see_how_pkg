a02d44c1ceb458f8fc107b24af9cf083c71bba2a
nata-vito <nata14vitorino@hotmail.com>
2023-03-21 18:25:22 -0300

    Mouse controller seperated from main program

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import math 
+import time
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
@@ -33,7 +34,9 @@ class handDetector():
         self.side               = ""  # Right/Left
         self.countFingers       = -1
         self.ids                = [4, 8, 12, 16, 20]
-        self.op                 = "" # To define what is the correct hant to detect
+        self.op                 = "" # To define what is the correct hand to detect
+        self.boundingBox        =  ''
+        self.tipIds = [4, 8, 12, 16, 20]
         
     # To find hands in video
     def findHands(self, img, op = "", draw=True):
@@ -55,54 +58,87 @@ class handDetector():
 
             return img
 
+    def findHandstoMouse(self, img, draw=True):
+        
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
     # Getting landmarks position
     def findPosition(self, img, handNo=0, draw=True):
-        self.lmList = []
-
+        xList           = []
+        yList           = []
+        bbox            = []
+        self.lmList     = []
+        
         if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-
-            # Hand Labeling
-            for id, hand_handedness in enumerate(self.results.multi_handedness):
-                self.label = hand_handedness.classification[0].label
-    
-            if self.label == self.op:
-                for id, lm in enumerate(myHand.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    self.lmList.append([id, cx, cy])
-                        
-                    # Drawing blue points
-                    if draw: cv.circle(img, (cx, cy), 5, (255, 0, 0), cv.FILLED)
-
-        self.handsLabel(self.lmList, self.ids)
+            myHand      = self.results.multi_hand_landmarks[handNo]
             
-        return self.lmList
+            for id, lm in enumerate(myHand.landmark):
+
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(img, (cx, cy), 5, (255, 0, 255), cv.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+
+    def fingersUp(self):
+        fingers = []
+        
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        
+        # Fingers
+        for id in range(1, 5):
+
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
     
-    def bounding_box(self, img, handNo=0, draw=True):
-        h, w, c = img.shape
-        if self.results.multi_hand_landmarks:
-            #myHand = self.results.multi_hand_landmarks[handNo]
-            myHand = self.results.multi_hand_landmarks
-            for handLMs in myHand:
-                x_max = 0
-                y_max = 0
-                x_min = w
-                y_min = h
-                for lm in handLMs.landmark:
-                    x, y = int(lm.x * w), int(lm.y * h)
-                    if x > x_max:
-                        x_max = x
-                    if x < x_min:
-                        x_min = x
-                    if y > y_max:
-                        y_max = y
-                    if y < y_min:
-                        y_min = y
-            coordinates = x_min, y_min, x_max, y_max
-        # if draw: cv.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-            print(coordinates)
-            return coordinates
+    
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+        
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv.circle(img, (x1, y1), r, (255, 0, 255), cv.FILLED)
+            cv.circle(img, (x2, y2), r, (255, 0, 255), cv.FILLED)
+            cv.circle(img, (cx, cy), r, (0, 0, 255), cv.FILLED)
+            
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
     
     # Getting the level output
     def levelOutput(self, frame):
@@ -180,7 +216,59 @@ class handDetector():
                     if self.fingers[i] == 1:
                         self.countFingers += 1
                 
-            
-            
             return self.side + " -> " + str(self.countFingers)
+    
+    # To draw the hand bounding box    
+    def bounding_box(self, img, handNo=0, draw=True, side = ''):
+        h, w, c = img.shape
         
+        if self.results.multi_hand_landmarks:
+            #myHand = self.results.multi_hand_landmarks[handNo]
+            myHand = self.results.multi_hand_landmarks
+            for handLMs in myHand:
+                x_max = 0
+                y_max = 0
+                x_min = w
+                y_min = h
+                for lm in handLMs.landmark:
+                    x, y = int(lm.x * w), int(lm.y * h)
+                    if x > x_max:
+                        x_max = x
+                    if x < x_min:
+                        x_min = x
+                    if y > y_max:
+                        y_max = y
+                    if y < y_min:
+                        y_min = y
+            coordinates = x_min, y_min, x_max, y_max
+            # if draw: cv.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            #print(coordinates)
+            return coordinates
+        
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv.VideoCapture(0)
+    detector = handDetector()
+    
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList, bbox = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        print(lmList)
+        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        cv.imshow("Image", img)
+        cv.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mouse.py
@@ -0,0 +1,145 @@
+import cv2
+import numpy as np
+import hand_tracking as htm
+import time
+import autopy, pyautogui
+    
+
+class Mouse:
+    
+    def __init__(self):
+        
+        self.wCam, self.hCam      = 640, 480
+        self.frameR               = 100            # Frame Reduction
+        self.smoothening          = 7
+
+        self.pTime                = 0
+        self.plocX, self.plocY    = 0, 0 # prev location mouse
+        self.clocX, self.clocY    = 0, 0 # current location mouse
+        self.active               = 0
+        self.controll_mode        = ''
+        self.fingers              = ''
+
+        self.cap                  = cv2.VideoCapture(0)
+        self.detector             = htm.handDetector(maxHands=1)
+        self.wScr, self.hScr      = autopy.screen.size()
+        self.lmList, self.bbox    = '', ''
+        self.x1, self.x2          = '', ''
+        self.y1, self.y2          = '', ''
+        self.fingers              = ''
+        self.controll_mode        = ''
+        self.active               = ''
+        self.img                  = ''
+
+        self.cap.set(3, self.wCam)
+        self.cap.set(4, self.hCam)
+        
+        pyautogui.FAILSAFE = False
+
+    def findHandLm(self):
+        
+        # 2. Get the tip of the index and middle fingers
+        if len(self.lmList) != 0:
+            self.x1, self.y1 = self.lmList[8][1:]
+            self.x2, self.y2 = self.lmList[12][1:]
+            
+            # 3. Check which fingers are up
+            self.fingers = self.detector.fingersUp()
+        
+    def decisionTaking(self):
+        
+        if (self.fingers == [0, 0, 0, 0, 0]):
+            self.controll_mode   = 'N'
+            self.active          = 0
+            
+        elif (self.fingers == [0, 1, 0, 0, 0] or self.fingers == [0, 1, 1, 0, 0]) & (self.active == 0):
+            self.controll_mode   = 'Scroll'
+            self.active          = 1
+        
+        elif (self.fingers == [1, 1, 1, 1, 1]) & (self.active == 0):
+            self.controll_mode   = 'Cursor'
+            self.active          = 1    
+    
+    def actingControlMode(self):
+        
+        if self.controll_mode == 'Cursor':
+            
+            # 4. Only Index Finger : Moving Mode
+            if self.fingers[1] == 1 and self.fingers[2] == 0:
+                
+                # 5. Convert Coordinates
+                cv2.rectangle(self.img, (self.frameR, self.frameR),  (self.wCam - self.frameR, self.hCam - self.frameR), (255, 0, 255), 2)
+                x3 = np.interp(self.x1, (self.frameR, self.wCam - self.frameR), (0, self.wScr))
+                y3 = np.interp(self.y1, (self.frameR, self.hCam - self.frameR), (0, self.hScr))
+            
+                # 6. Smoothen Values
+                clocX = self.plocX + (x3 - self.plocX) / self.smoothening
+                clocY = self.plocY + (y3 - self.plocY) / self.smoothening
+                
+                # 7. Move Mouse
+                autopy.mouse.move(self.wScr - clocX, clocY)
+                cv2.circle(self.img, (self.x1, self.y1), 15, (255, 0, 255), cv2.FILLED)
+                self.plocX, self.plocY = clocX, clocY
+                
+            # 8. Both Index and middle fingers are up : Clicking Mode
+            if self.fingers[1] == 1 and self.fingers[2] == 1:
+                
+                # 9. Find distance between fingers
+                length, img, lineInfo = self.detector.findDistance(8, 12, self.img)
+                print(length)
+
+                # 10. Click mouse if distance short
+                if length < 30:
+                    cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                    autopy.mouse.click()
+                    
+        elif self.controll_mode == 'Scroll':
+            
+            cv2.rectangle(self.img, (200, 410), (245, 460), (255, 255, 255), cv2.FILLED)
+            
+            if len(self.lmList) != 0:
+                
+                if self.fingers == [0,1,0,0,0]:
+                    pyautogui.scroll(1)
+
+                if self.fingers == [0,1,1,0,0]:
+                    pyautogui.scroll(-1)
+                    
+                elif self.fingers == [0, 0, 0, 0, 0]:
+                    active = 0
+                    mode = 'N'
+                    
+                    
+                 
+    def main(self):
+        
+        while True:
+        
+            # 1. Find hand Landmarks
+            success, self.img       = self.cap.read()
+            self.img                = self.detector.findHandstoMouse(self.img)
+            self.lmList, self.bbox  = self.detector.findPosition(self.img)
+            
+            self.findHandLm()
+            self.decisionTaking()
+            self.actingControlMode()
+
+            print(self.controll_mode, self.active)
+            
+            # 11. Frame Rate
+            cTime = time.time()
+            fps = 1 / (cTime - self.pTime)
+            self.pTime = cTime
+            cv2.putText(self.img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+            
+            # 12. Display
+            cv2.imshow("Image", self.img)
+            cv2.waitKey(1)
+            
+                     
+           
+if __name__ == '__main__':
+    mouse = Mouse()
+    mouse.main()
+else:
+    pass
