2954d12d6467473ed8edad5ca35b04a6ea3b8eff
nata-vito <nata14vitorino@hotmail.com>
2023-03-22 21:33:13 +0000

    distance measurement of the developing hand

--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import hand_tracking
 
 cap = cv2.VideoCapture(0)
@@ -9,6 +10,16 @@ detector = hand_tracking.handDetector(detectionCon = 0.8, maxHands = 1)
 
 while True:
     success, img = cap.read()
-    img = detector.findHands(img)
+    hands, img = detector.findHands(img)
+    
+    if hands:
+        lmList = hands[0]['lmList']
+        print(lmList)
+        
+        x1, y1 = lmList[5]
+        x2, y2 = lmList[17]
+        
+        print(abs(x2 - x1))
+        
     cv2.imshow('Cam', img)
     cv2.waitKey(1)
