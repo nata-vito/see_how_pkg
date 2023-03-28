5bc7ff005ae724156c03b91b96eeb2b586bf49eb
nata-vito <nata14vitorino@hotmail.com>
2023-03-28 17:34:19 +0000

    Up-to-date library with the requirements for mouse operation

--- src/scripts/hand_tracking.py
+++ src/scripts/hand_tracking.py
@@ -34,7 +34,8 @@ class handDetector():
         self.side               = ""  # Right/Left
         self.countFingers       = -1
         self.ids                = [4, 8, 12, 16, 20]
-        self.op                 = "" # To define what is the correct hant to detect
+        self.op                 = "" # To define what is the correct hand to detect
+        self.boundingBox        =  ''
         
     # To find hands in video
     def findHands(self, img, op = "", draw=True):
@@ -55,6 +56,113 @@ class handDetector():
                     if draw: self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
             return img
+
+    def findHandstoMouse(self, img, draw=True):
+        
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    # Getting landmarks position
+    def findMousePosition(self, img, handNo=0, draw=True):
+        xList           = []
+        yList           = []
+        bbox            = []
+        self.lmList     = []
+        
+        if self.results.multi_hand_landmarks:
+            myHand      = self.results.multi_hand_landmarks[handNo]
+            
+            for id, lm in enumerate(myHand.landmark):
+
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(img, (cx, cy), 5, (255, 0, 255), cv.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+
+    def fingersUp(self):
+        fingers = []
+        
+        # Thumb
+        if self.lmList[self.ids[0]][1] > self.lmList[self.ids[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        
+        # Fingers
+        for id in range(1, 5):
+
+            if self.lmList[self.ids[id]][2] < self.lmList[self.ids[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
+    
+    
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+        
+        if self.lmList:
+            x1, y1 = self.lmList[p1][1:]
+            x2, y2 = self.lmList[p2][1:]
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+            if draw:
+                cv.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+                cv.circle(img, (x1, y1), r, (255, 0, 255), cv.FILLED)
+                cv.circle(img, (x2, y2), r, (255, 0, 255), cv.FILLED)
+                cv.circle(img, (cx, cy), r, (0, 0, 255), cv.FILLED)
+                
+            length = math.hypot(x2 - x1, y2 - y1)
+
+            return length, img, [x1, y1, x2, y2, cx, cy]
+        else:
+            #print('Unidentified Hand ')
+            return 0, img, [0, 0, 0, 0, 0, 0]
+    
+    # Getting the level output
+    def levelOutput(self, frame):
+
+        if len(self.lmList) != 0:
+            x1, y1 = self.lmList[4][1], self.lmList[4][2]               # Thumb tip circle
+            x2, y2 = self.lmList[8][1], self.lmList[8][2]               # Index tip circle
+            cx, cy = (x1 + x2) // 2, (y1 + y2) //2                      # Middle circle
+
+            cv.circle(frame, (x1, y1), 10, (255, 0, 255), cv.FILLED)
+            cv.circle(frame, (x2, y2), 10, (255, 0, 255), cv.FILLED)
+            cv.line(frame, (x1, y1), (x2,y2), (255,0,255), 3)
+            cv.circle(frame, (cx, cy), 10, (255, 0, 255), cv.FILLED)
+
+            self.length = math.hypot(x2-x1, y2-y1)
+            self.length = int(np.interp(self.length, [30, 230], [0, 100]))
+
+            if self.length < 20:
+                cv.circle(frame, (cx, cy), 10, (0, 255, 0), cv.FILLED)
+            
+            return self.length
 
     # Getting landmarks position
     def findPosition(self, img, handNo=0, draw=True):
